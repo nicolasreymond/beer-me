5b46b3d14760380ecd2ae781686352a1b7c4f6c6
Nicolas Reymond <n.reymond.12@gmail.com>
2020-07-02 11:04:41 +0200

    First commit

---
+++ src/components/News.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{ msg }}
+    <SimpleComponent />
+  </div>
+</template>
+
+<script>
+import SimpleComponent from "./SimpleComponent.vue";
+
+export default {
+  name: "app",
+  components: {
+    SimpleComponent
+  },
+
+  data() {
+    return {
+      msg: "My Profile Component"
+    };
+  }
+};
+</script>
