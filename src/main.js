20f5191b6b7ad1f5021420ab3fc1ff2618b4c0ad
Nicolas Reymond <n.reymond.12@gmail.com>
2020-07-02 16:00:49 +0200

    [ADD] Some imports

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
