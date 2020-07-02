4e24e1bfba86450ace3fe3caf12742869d6728f5
Nicolas Reymond <n.reymond.12@gmail.com>
2020-07-02 15:58:04 +0200

    [ADD] Add some new pages

---
+++ src/components/ContactPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Contact Me</h1>
+  <p>
+    <ul>
+      <li>Email: {{email}}</li>
+      <li>Website: <a href="">{{web}}</a></li>
+    </ul>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data(){
+    return {
+      email: "n.reymond.12@gmail.com",
+      web: ""
+    }
+  }
+}
+</script>
