002c7b765c9b252f184153c0489c1de2160c1a96
TacticsCH <alexandre.javet@hotmail.com>
2020-07-07 15:21:06 +0200

    [ADD] Added a homepage presentation
    - BeerMe logo has been aded to the presentation
    - b-media has been used to do the presentation

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,98 +1,24 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
-</template>
+  <div class="home-page">
+    <div class="presentation">
+      <b-card>
+        <b-media>
+          <template v-slot:aside>
+            <b-img :src="require('../assets/BeerMe.svg')" width="64" alt="placeholder"></b-img>
+          </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome nreymond 1413 to Your Vue.js App'
-    }
-  }
-}
-</script>
+          <h2 class="mt-0">Présentation de BeerMe</h2>
+          <p>
+            I'm baby bespoke shabby chic lyft, brunch cold-pressed migas tbh pour-over raclette forage trust fund pop-up flexitarian echo park quinoa. Marfa scenester offal la croix wayfarers try-hard street art migas sartorial cliche. Bushwick ramps man bun church-key. Austin enamel pin meggings, banh mi skateboard semiotics pork belly kale chips waistcoat venmo flexitarian four dollar toast tumblr hoodie art party.<br/>
+
+            Bushwick small batch raw denim pop-up, trust fund franzen raclette pork belly air plant whatever iPhone disrupt. Disrupt raw denim post-ironic, put a bird on it cronut tilde shoreditch pickled health goth lumbersexual palo santo chia fanny pack microdosing try-hard. Vaporware intelligentsia hell of narwhal lyft, drinking vinegar kogi messenger bag. 3 wolf moon yuccie helvetica food truck, mixtape tofu single-origin coffee butcher whatever actually keffiyeh authentic tumblr meggings. Roof party portland man braid air plant distillery vegan la croix.<br/>
+$
+            DIY mustache tote bag godard retro gluten-free copper mug schlitz crucifix taxidermy hella before they sold out VHS blog vape. Enamel pin actually brunch literally godard. Mumblecore meditation forage, coloring book ethical single-origin coffee fingerstache mlkshk pork belly leggings pour-over. Pabst meggings offal post-ironic fingerstache.
+          </p>
+        </b-media>
+      </b-card>
+    </div>
+</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
