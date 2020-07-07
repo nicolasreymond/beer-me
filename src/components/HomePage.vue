87c2a5098b8a89b0750a1a4763d171580bb0e69d
TacticsCH <alexandre.javet@hotmail.com>
2020-07-07 15:44:10 +0200

    [ADD] Beers of the month display
    - Shown as a side bar
    - Is called via a button placed right bellow the presentation

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -16,8 +16,32 @@ $
             DIY mustache tote bag godard retro gluten-free copper mug schlitz crucifix taxidermy hella before they sold out VHS blog vape. Enamel pin actually brunch literally godard. Mumblecore meditation forage, coloring book ethical single-origin coffee fingerstache mlkshk pork belly leggings pour-over. Pabst meggings offal post-ironic fingerstache.
           </p>
         </b-media>
+        <b-button v-b-toggle.display-beer-of-the-month>Bières du mois !</b-button>
       </b-card>
     </div>
+
+    <div class="beer-of-the-month">
+        <b-sidebar id="display-beer-of-the-month" title="Bières du mois" right shadow>
+          <div class="px-3 py-2">
+            <h5>Bière Lorem</h5>
+            <p>
+              Taxidermy readymade pop-up jianbing lomo bitters green juice church-key live-edge offal cardigan small batch shaman forage umami. Actually banjo neutra flexitarian, celiac mustache vinyl literally intelligentsia. Art party listicle edison bulb yuccie, asymmetrical flannel twee ugh shoreditch celiac.
+            </p>
+
+            <h5>Bière Ipsum</h5>
+            <p>
+              Lyft mustache pok pok gentrify. Freegan before they sold out kombucha drinking vinegar paleo vape wolf knausgaard. Intelligentsia austin narwhal drinking vinegar selfies activated charcoal hot chicken blue bottle chicharrones hexagon slow-carb. Pop-up vice woke, unicorn vinyl hell of fam leggings before they sold out cornhole craft beer.
+            </p>
+
+            <h5>Bière Leffe</h5>
+            <p>
+              Wayfarers chartreuse distillery, succulents food truck mixtape disrupt forage asymmetrical. Pour-over pickled flannel vegan listicle bushwick fanny pack DIY edison bulb bespoke kinfolk cold-pressed. Drinking vinegar pour-over artisan truffaut chartreuse copper mug banh mi keffiyeh.
+            </p>
+
+          </div>
+        </b-sidebar>
+    </div>
+  </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
