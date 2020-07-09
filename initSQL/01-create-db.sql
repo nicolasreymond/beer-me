2dd84c5558aa9227a5c7407ee6132dacc806ccdc
Nicolas Reymond <n.reymond.12@gmail.com>
2020-07-09 15:12:46 +0200

    [RFA] Remane DB

--- initSQL/01-create-db.sql
+++ initSQL/01-create-db.sql
@@ -1,6 +1,6 @@
 SET NAMES utf8;
 SET time_zone = '+02:00';
 SET sql_mode = '';
-DROP DATABASE IF EXISTS `kata-api`;
-CREATE DATABASE `kata-api` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `kata-api`;
+DROP DATABASE IF EXISTS `beer-me`;
+CREATE DATABASE `beer-me` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `beer-me`;
