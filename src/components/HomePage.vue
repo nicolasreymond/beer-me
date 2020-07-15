5fbaae480f5f5e7f8e1c4c083e4d4c2572195419
Nicolas Reymond <n.reymond.12@gmail.com>
2020-07-15 13:16:02 +0200

    [FIX] Fix carousel and add style to it

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,12 +3,9 @@
     <div class="beer-of-the-month">
       <b-carousel
         id="carousel-fade"
-        style="text-shadow: 0px 0px 2px #000"
         fade
-        interval="3000"
+        interval="2000"
         indicators
-        img-width="1024"
-        img-height="480"
       >
         <b-carousel-slide
           caption="Promo du mois !"
@@ -24,19 +21,16 @@
         ></b-carousel-slide>
       </b-carousel>
     </div>
-
     <div class="presentation">
       <b-card>
         <b-media>
-          <template v-slot:aside>
-          </template>
+          <template v-slot:aside></template>
 
           <h1 class="mt-0">Présentation de BeerMe</h1>
           <p>
-            I'm baby bespoke shabby chic lyft, brunch cold-pressed migas tbh pour-over raclette forage trust fund pop-up flexitarian echo park quinoa. Marfa scenester offal la croix wayfarers try-hard street art migas sartorial cliche. Bushwick ramps man bun church-key. Austin enamel pin meggings, banh mi skateboard semiotics pork belly kale chips waistcoat venmo flexitarian four dollar toast tumblr hoodie art party.<br/>
-
-            Bushwick small batch raw denim pop-up, trust fund franzen raclette pork belly air plant whatever iPhone disrupt. Disrupt raw denim post-ironic, put a bird on it cronut tilde shoreditch pickled health goth lumbersexual palo santo chia fanny pack microdosing try-hard. Vaporware intelligentsia hell of narwhal lyft, drinking vinegar kogi messenger bag. 3 wolf moon yuccie helvetica food truck, mixtape tofu single-origin coffee butcher whatever actually keffiyeh authentic tumblr meggings. Roof party portland man braid air plant distillery vegan la croix.<br/>
-$50
+            I'm baby bespoke shabby chic lyft, brunch cold-pressed migas tbh pour-over raclette forage trust fund pop-up flexitarian echo park quinoa. Marfa scenester offal la croix wayfarers try-hard street art migas sartorial cliche. Bushwick ramps man bun church-key. Austin enamel pin meggings, banh mi skateboard semiotics pork belly kale chips waistcoat venmo flexitarian four dollar toast tumblr hoodie art party.
+            <br />Bushwick small batch raw denim pop-up, trust fund franzen raclette pork belly air plant whatever iPhone disrupt. Disrupt raw denim post-ironic, put a bird on it cronut tilde shoreditch pickled health goth lumbersexual palo santo chia fanny pack microdosing try-hard. Vaporware intelligentsia hell of narwhal lyft, drinking vinegar kogi messenger bag. 3 wolf moon yuccie helvetica food truck, mixtape tofu single-origin coffee butcher whatever actually keffiyeh authentic tumblr meggings. Roof party portland man braid air plant distillery vegan la croix.
+            <br />$50
             DIY mustache tote bag godard retro gluten-free copper mug schlitz crucifix taxidermy hella before they sold out VHS blog vape. Enamel pin actually brunch literally godard. Mumblecore meditation forage, coloring book ethical single-origin coffee fingerstache mlkshk pork belly leggings pour-over. Pabst meggings offal post-ironic fingerstache.
           </p>
         </b-media>
@@ -45,6 +39,7 @@ $50
   </div>
 </template>
 
+ 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
@@ -65,5 +60,9 @@ a {
 }
 p {
   font-family: Lekton;
+}
+.carousel-item {
+  height: 700px;
+  text-shadow: 0px 0px 2px #000;
 }
 </style>
