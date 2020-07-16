12dda4cf62594e2bb4faf013039c76e9a7517ed5
TacticsCH <alexandre.javet@hotmail.com>
2020-07-16 13:41:33 +0200

    [ADD] Title has changed
    - Title now matches our template

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="home-page">
     <div class="beer-of-the-month">
-      <b-carousel
-        id="carousel-fade"
-        fade
-        interval="2000"
-        indicators
-      >
+      <b-carousel id="carousel-fade" fade interval="2000" indicators>
         <b-carousel-slide
           caption="Promo du mois !"
           img-src="https://picsum.photos/1024/480/?image=10"
@@ -26,7 +21,11 @@
         <b-media>
           <template v-slot:aside></template>
 
-          <h1 class="mt-0">Présentation de BeerMe</h1>
+          <h1 class="mt-0">
+            <span class="underline">
+              <span class="stabilo">Présentation de BeerMe</span>
+            </span>
+          </h1>
           <p>
             I'm baby bespoke shabby chic lyft, brunch cold-pressed migas tbh pour-over raclette forage trust fund pop-up flexitarian echo park quinoa. Marfa scenester offal la croix wayfarers try-hard street art migas sartorial cliche. Bushwick ramps man bun church-key. Austin enamel pin meggings, banh mi skateboard semiotics pork belly kale chips waistcoat venmo flexitarian four dollar toast tumblr hoodie art party.
             <br />Bushwick small batch raw denim pop-up, trust fund franzen raclette pork belly air plant whatever iPhone disrupt. Disrupt raw denim post-ironic, put a bird on it cronut tilde shoreditch pickled health goth lumbersexual palo santo chia fanny pack microdosing try-hard. Vaporware intelligentsia hell of narwhal lyft, drinking vinegar kogi messenger bag. 3 wolf moon yuccie helvetica food truck, mixtape tofu single-origin coffee butcher whatever actually keffiyeh authentic tumblr meggings. Roof party portland man braid air plant distillery vegan la croix.
@@ -60,6 +59,14 @@ a {
 }
 p {
   font-family: Lekton;
+}
+span.stabilo {
+  padding-right: 10px;
+  background: linear-gradient(0deg, #ffe300 50%, #fff0 50%);
+}
+span.underline {
+  padding-left: 70px;
+  border-bottom: 5px solid #000;
 }
 .carousel-item {
   height: 700px;
