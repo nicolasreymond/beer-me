b31a39a961c59d49018e151ea8aacea33be50866
Nicolas Reymond <n.reymond.12@gmail.com>
2020-07-21 14:37:26 +0200

    [F.T] Create abonnement model

---
+++ api/models/utilisateur-brasserieModel.php
@@ -0,0 +1,63 @@
+<?php
+
+    class abonnement
+    {
+        private $idUTILISATEUR_BRASSERIE;
+        private $FK_idUTILISATEUR;
+        private $FK_idBRASSERIE;
+        private $dateCreation;
+
+        public function __construct($idUTILISATEUR_BRASSERIE, $FK_idUTILISATEUR, $FK_idBRASSERIE, $dateCreation)
+        {
+            $this->$idUTILISATEUR_BRASSERIE = $idUTILISATEUR_BRASSERIE;
+            $this->$FK_idUTILISATEUR = $FK_idUTILISATEUR;
+            $this->$FK_idBRASSERIE = $FK_idBRASSERIE;
+            $this->$dateCreation = $dateCreation;
+        }
+
+        /***
+         * getters
+         */
+        public function getidUTILISATEUR_BRASSERIE()
+        {
+            return $this->$idUTILISATEUR_BRASSERIE;
+        }
+            
+        public function getFK_idUTILISATEUR()
+        {
+            return $this->$FK_idUTILISATEUR;
+        }
+        
+        public function getFK_idBRASSERIE()
+        {
+            return $this->$FK_idBRASSERIE;
+        }
+        
+        public function getDateCreation()
+        {
+            return $this->$dateCreation;
+        }
+        
+        /***
+         * setters
+         */
+        public function setidUTILISATEUR_BRASSERIE($id)
+        {
+            $this->$idUTILISATEUR_BRASSERIE = $id;
+        }
+            
+        public function setFK_idUTILISATEUR($FK_UTILISATEUR)
+        {
+            $this->$FK_idUTILISATEUR = $FK_UTILISATEUR;
+        }
+        
+        public function setFK_idBRASSERIE($FK_BRASSERIE)
+        {
+            $this->$FK_idBRASSERIE = $FK_BRASSERIE;
+        }
+        
+        public function setdateCreation($date)
+        {
+            $this->$dateCreation = $date;
+        }
+    }
