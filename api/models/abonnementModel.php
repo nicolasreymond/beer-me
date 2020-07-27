0f95bb9397578a61eb491acdb54e681cdccfec09
Luca Landry <lucalandry@icloud.com>
2020-07-27 09:13:16 +0200

    [FIX] all database communication in model abonnement

--- api/models/abonnementModel.php
+++ api/models/abonnementModel.php
@@ -2,86 +2,117 @@
 
     class abonnementModel
     {
-        private $idABONNEMENT;
+        private $idABONNEMENT
         private $nomAbonnement;
         private $descriptionAbonnement;
         private $dureeAbonnement;
         private $nbrBoissonAbonnement;
         private $prixMensuelAbonnement;
 
-        public function __construct($idABONNEMENT, $nomAbonnement, $descriptionAbonnement, $dureeAbonnement, $nbrBoissonAbonnement, $prixMensuelAbonnement)
+        public function __construct($nomAbonnement, $descriptionAbonnement, $dureeAbonnement, $nbrBoissonAbonnement, $prixMensuelAbonnement)
         {
-            $this->$idABONNEMENT = $idABONNEMENT;
             $this->$nomAbonnement = $nomAbonnement;
             $this->$descriptionAbonnement = $descriptionAbonnement;
             $this->$dureeAbonnement = $dureeAbonnement;
             $this->$nbrBoissonAbonnement = $nbrBoissonAbonnement;
             $this->$prixMensuelAbonnement = $prixMensuelAbonnement;
+
+          try {
+            $sql = "INSERT INTO ABONNEMENT (nomAbonnement, 
+                descriptionAbonnement, 
+                dureeAbonnement, 
+                nbrBoissonAbonnement, 
+                prixMensuelAbonnement)
+                    VALUE (:nom, 
+                :desc, 
+                :duree, 
+                :nbrBoisson, 
+                :prixMensuel);";
+            $stmt = EDatabase::prepare($sql);
+            $stmt->bindParam(':nom', $nomAbonnement, PDO::PARAM_STR);
+            $stmt->bindParam(':desc', $descriptionAbonnement, PDO::PARAM_STR);
+            $stmt->bindParam(':duree', $dureeAbonnement, PDO::PARAM_INT);
+            $stmt->bindParam(':nbrBoisson', $nbrBoissonAbonnement, PDO::PARAM_INT);
+            $stmt->bindParam(':prixMensuel', $prixMensuelAbonnement, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $this->idABONNEMENT = EDatabase::lastInsertId();
+            return true;
+          } catch (Exception $e) {
+            EDatabase::rollBack();
+            echo "erreur de création d'abonnement : " . $e;
+            return false;
+          }
         }
 
-        /***
-         * getters
-         */
-        public function getIdABONNEMENT()
-        {
-            return $this->$idABONNEMENT;
-        }
-            
-        public function getNomAbonnement()
-        {
-            return $this->$nomAbonnement;
-        }
-        
-        public function getDescriptionAbonnement()
-        {
-            return $this->$descriptionAbonnement;
-        }
-        
-        public function getDureeAbonnement()
-        {
-            return $this->$dureeAbonnement;
-        }
-        
-        public function getNbrBoissonAbonnement()
-        {
-            return $this->$nbrBoissonAbonnement;
-        }
-        
-        public function getPrixMensuelAbonnement()
-        {
-            return $this->$prixMensuelAbonnement;
+      public function getAllAbonnements()
+      {
+        $abonnements = array();
+
+        try {
+          $sql = "SELECT * FROM ABONNEMENT;";
+          $stmt = EDatabase::prepare($sql);
+          $results = $stmt->fetchAll();
+        } catch (Exception $e) {
+          EDatabase::rollBack();
+          echo "erreur lors du get all abonnements : " . $e;
+          return false;
         }
 
-        /***
-         * setters
-         */
-        public function setIdABONNEMENT($id)
-        {
-            $this->$idABONNEMENT = $id;
+        return $abonnements;
+      }
+
+      public function getAbonnementById ($id){
+        try{
+          $sql = "SELECT * FROM ABONNEMENT
+                WHERE idABONNEMENT = :id;";
+          $stmt = EDatabase::prepare($sql);
+          $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+          $result = $stmt->fetch(PDO::FETCH_OBJ);
+        }catch (Exception $e){
+          EDatabase::rollBack();
+          echo "problème sur le get d'un abo par l'id : " . $e;
+          return false;
         }
-            
-        public function setNomAbonnement($nom)
-        {
-            $this->$nomAbonnement = $nomAbonnement;
+      }
+
+      public function delAbonnement ($id){
+        try{
+          $sql = "DELETE FROM ABONNEMENT WHERE id = :id;";
+          $stmt = EDatabase::prepare($sql);
+          $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        }catch (Exception $e){
+          EDatabase::rollBack();
+          echo "problème sur le delete d'un abo par l'id : " . $e;
+          return false;
         }
-        
-        public function setDescriptionAbonnement($description)
-        {
-            $this->$descriptionAbonnement = $description;
+
+        return true;
+
+      }
+
+      public function updateAbonnement($id, $nom, $description, $dureeabonnement, $nbrBoisson, $prixMensuel){
+        try {
+          $sql = "UPDATE ABONNEMENT set nomAbonnement = :nom, 
+            set descriptionAbonnement = :desc, 
+            set dureeAbonnement = :duree, 
+            set nbrBoissonAbonnement = :nbrBoisson, 
+            set prixMensuelAbonnement = :prixMensuel)
+                WHERE idABONNEMENT = :id;";
+          $stmt = EDatabase::prepare($sql);
+          $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+          $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
+          $stmt->bindParam(':desc', $description, PDO::PARAM_STR);
+          $stmt->bindParam(':duree', $dureeabonnement, PDO::PARAM_INT);
+          $stmt->bindParam(':nbrBoisson', $nbrBoisson, PDO::PARAM_INT);
+          $stmt->bindParam(':prixMensuel', $prixMensuel, PDO::PARAM_INT);
+          $stmt->execute();
+
+          return true;
+        } catch (Exception $e) {
+          EDatabase::rollBack();
+          echo "erreur d'update d'abonnement : " . $e;
+          return false;
         }
-        
-        public function setDureeAbonnement($duree)
-        {
-            $this->$dureeAbonnement = $duree;
-        }
-        
-        public function setNbrBoissonAbonnement($nbrBoisson)
-        {
-            $this->$nbrBoissonAbonnement = $nbrBoisson;
-        }
-        
-        public function setPrixMensuelAbonnement($prixMensuel)
-        {
-            $this->$prixMensuelAbonnement = $prixMensuel;
-        }
+      }
     }
